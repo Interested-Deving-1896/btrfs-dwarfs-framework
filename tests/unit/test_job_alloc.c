59942cb0d26de3b2797122e8f06f0a83b74d29f9
Interested Deving <interested.deving.1896@gmail.com>
2026-03-29 11:45:10 -0700

    fix: remove open() macro stub (now handled via BDFS_UNIT_TEST in bdfs_daemon.c)

--- tests/unit/test_job_alloc.c
+++ tests/unit/test_job_alloc.c
@@ -19,11 +19,8 @@ static int stub_socket_init_called  = 0;
 #define main bdfs_daemon_main_unused
 #define bdfs_netlink_init(d) (stub_netlink_init_called++, (d)->nl_fd = -1, 0)
 #define bdfs_socket_init(d)  (stub_socket_init_called++,  (d)->sock_fd = -1, 0)
-#undef open
-#define open(path, flags, ...) (-1)
 #include "../../userspace/daemon/bdfs_daemon.c"
 #undef main
-#undef open
 
 static int tests_run = 0, tests_failed = 0;
 
